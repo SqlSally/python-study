92057b31d0531b211f5a99eed74400ba5dd22cf1
Sally <songqi26@outlook.com>
2017-09-14 15:51:21 -0400

    filter_and_map

--- 21course.py
+++ 21course.py
@@ -9,5 +9,8 @@ def odd(x):
 temp = range(10)
 show = filter(odd, temp)
 print(list(show))
-'''
+
 print(list(filter(lambda x : x % 2, range(10))))
+'''
+
+print(list(map(lambda x : x * 2 , range(10))))
