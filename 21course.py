300f02264bd24ec083ad1ab650cd82a2c08e8d2e
Sally <songqi26@outlook.com>
2017-09-14 15:43:54 -0400

    Find_Space_Digit_alpha_In_Str

---
+++ 21course.py
@@ -0,0 +1,13 @@
+'''
+g=list(filter(None, [1,0, False, True]))
+print(g)
+
+
+def odd(x):
+    return x % 2
+
+temp = range(10)
+show = filter(odd, temp)
+print(list(show))
+'''
+print(list(filter(lambda x : x % 2, range(10))))
