0a1ee9f834497572539b65ecee6f0cfb6796f0b9
Sally <songqi26@outlook.com>
2017-09-21 18:27:01 -0400

    dictionary_key_value

--- dictionary.py
+++ dictionary.py
@@ -11,7 +11,6 @@ print("my slogan is ", slogan[brand.index("just do it")])
 dic1 = {"a": "everything is possible", "b": "just do it" ,"c":"haha", "d": "i am d"}     #字典为映射类型
 print(dic1["a"])
 
-'''
 
 dic3 = dict((("F", 70),("i",105)))
 
@@ -24,3 +23,16 @@ print(dict4)
 dict4[ "songqi"] = "i hate you"
 
 print(dict4)
+'''
+dict1 = {}
+dict1 = dict1.fromkeys(range(20), "")  #fromkeys
+
+for eachkey in dict1.items(): #items
+    print(eachkey)
+
+print(dict1.get(32)) #get
+
+dict1.clear()  #clear
+print(dict1)
+
+help( dict)
