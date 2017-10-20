dd722f1f41556b3c2c52ccd4c9d6ecef406ff685
Sally <songqi26@outlook.com>
2017-10-20 11:44:15 -0400

    related_class

--- test.py
+++
@@ -1,4 +0,0 @@
-brand = ["a", "b", "c", "d"]
-slogan = ["everything is possible", "just do it", "haha", " i am d"]
-
-print("my slogan is ", slogan[brand.index("a")])
--- triple.py
+++
@@ -1,7 +0,0 @@
-a, b, x, y, z = 6, 5, 4, 1, 0
-
-def min(x, y):
-    return x if x < y else y
-
-small = min(min(min(min(x, y), z), a), b)
-print(small)
